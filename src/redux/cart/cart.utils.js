8db43ef39fa4528337fce33ef1695e9dee574025
Paul Gates <pgates@paychex.com>
2021-02-01 10:31:48 -0500

    Fixed ids in shop data, updated cart utils to group by id

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,18 @@
+import logger from "redux-logger";
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(
+      cartItem => cartItem.id === cartItemToAdd.id
+    );
+  
+    if (existingCartItem) {
+        console.log('Inside if block');
+        return cartItems.map(cartItem =>
+        cartItem.id === cartItemToAdd.id ?
+            {...cartItem, quantity: cartItem.quantity + 1 } : 
+            cartItem
+      );
+    }
+  
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  };
